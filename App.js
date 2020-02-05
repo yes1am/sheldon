79844f0abe44b71b848aba4167fe09479a0d72d7
Jianping Song <jianping.song@shopee.com>
2020-02-05 23:41:31 +0800

    feat: 支持链接分享

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import axios from 'axios';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,100 +15,184 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
+  Button,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+async function getTitleFromUrl(url) {
+  if (!url) {
+    return '';
+  }
+  try {
+    const res = await axios({
+      url,
+      method: 'get',
+    });
+    if (res && res.data) {
+      const title = res.data.match('<title.*>(.*)</title>')[1];
+      return title;
+    }
+    return '';
+  } catch (e) {
+    console.log(`getTitleFromUrl: ${url} error,`, e);
+    return '';
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sharedUrl: '',
+      token: '',
+      info: '',
+    };
+    this.timer = null;
+    const {sharedUrl} = this.props;
+    if (sharedUrl) {
+      this.saveUrlRemote();
+    }
+  }
+
+  componentDidMount() {
+    this.getStorage('token').then(token => {
+      this.setState({token});
+    });
+  }
+
+  async saveUrlRemote() {
+    const {sharedUrl} = this.props;
+    const token = await this.getStorage('token');
+    if (!token) {
+      return;
+    }
+    console.log('start saveUrlRemote:', sharedUrl);
+    const title = await getTitleFromUrl(sharedUrl);
+    if (!title) {
+      return;
+    }
+    axios({
+      method: 'post',
+      url: 'https://api.github.com/repos/yes1am/PiggyBank/issues',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `token ${token}`,
+      },
+      data: {
+        title,
+        body: sharedUrl,
+      },
+    })
+      .then(response => {
+        if (response.data) {
+          console.log('success');
+          this.showSuccessInfo('发送到 github 成功');
+        }
+      })
+      .catch(err => {
+        console.log('err', err);
+        this.showSuccessInfo('发送到 github 失败');
+      });
+  }
+
+  async setStorage({key, value}, callback) {
+    try {
+      await AsyncStorage.setItem(key, value, () => {
+        if (callback) {
+          callback();
+        }
+      });
+    } catch (error) {
+      console.log(`setStorage error, for key=${key}, value=${value}`);
+    }
+  }
+
+  async getStorage(key) {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        return value;
+      }
+    } catch (error) {
+      console.log(`getStorage error, for key=${key}`);
+    }
+  }
+
+  onChangeToken(token) {
+    this.setState({token});
+  }
+
+  onSaveToken() {
+    const {token} = this.state;
+    this.setStorage({key: 'token', value: token}, () => {
+      this.showSuccessInfo('保存 token 成功');
+    });
+  }
+
+  showSuccessInfo(successInfo = 'success', duration = 1500) {
+    this.setState({
+      info: successInfo,
+    });
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this.setState({
+        info: '',
+      });
+    }, duration);
+  }
+
+  render() {
+    const {sharedUrl, token, info} = this.state;
+    return (
+      <>
+        {/* 状态栏，信号，电量 */}
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <TextInput
+              style={styles.tokenInput}
+              onChangeText={token => this.onChangeToken(token)}
+              placeholder="token"
+              value={token}
+            />
+            <View style={styles.body}>
+              {sharedUrl ? (
+                <Text> receive {sharedUrl}</Text>
+              ) : (
+                <Text> 暂无分享链接 </Text>
+              )}
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+            {!!info && <Text> {info} </Text>}
+            <Button
+              onPress={() => this.onSaveToken()}
+              title="save token"
+              color="#841584"
+            />
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
+    backgroundColor: '#F3F3F3',
   },
   body: {
-    backgroundColor: Colors.white,
+    backgroundColor: '#FFF',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  center: {
+    textAlign: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  tokenInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
 
--- android/app/src/main/java/com/sheldon/MainActivity.java
+++ android/app/src/main/java/com/sheldon/MainActivity.java
@@ -1,6 +1,10 @@
 package com.sheldon;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import android.os.Bundle;
+import javax.annotation.Nullable;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,19 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "sheldon";
+  }
+  
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Nullable
+        @Override
+        protected Bundle getLaunchOptions() {
+            Intent intent = MainActivity.this.getIntent();
+            Bundle bundle = new Bundle();
+            bundle.putString("sharedUrl", intent.getStringExtra(Intent.EXTRA_TEXT));
+            return bundle;
+        }
+    };
   }
 }
